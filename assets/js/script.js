0ed20f2ac5301b1ebda2c896bff0773fb5ee386f
Bogdan Viorel Burlă <bogdanviorel13@gmail.com>
2023-04-26 20:17:13 +0000

    write main function in JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,63 +2,180 @@
 //Make sure that DOM is loading before the game
 //Get the cards and messages
 
-document.addEventListener("DOMContentLoaded", function () {
-  let overlayText = document.getElementsByClassName("overlay-text");
-  overlayText.classList.remove("visible");
+function startGame(cardsArray, totalTime) {
+  // Initialize variables to track game state
+  let totalClicks = 0;
+  let timeRemaining = totalTime;
+  let cardToCheck = null;
+  let matchedCards = [];
+  let busy = true;
 
-  //   overlayText.addEventListener("click", function () {
-  //     alert("You cliked a card!");
-  //   });
+  // Get references to DOM elements
+  const timer = document.getElementById("time-remaining");
+  const ticker = document.getElementById("flips");
 
-  let cards = document.getElementsByClassName("card");
-  for (let card of cards) {
-    card.addEventListener("click", function () {
-      if (this.getAttribute("data-type") === "card")
-        alert("You cliked a card!");
+  // Shuffle cards after a delay and start countdown timer
+  setTimeout(() => {
+    shuffleCards(cardsArray);
+    countdown = startCountdown();
+    busy = false;
+  }, 500);
+
+  // Hide all cards at the start of the game
+  hideCards(cardsArray);
+
+  // Set initial values for the timer and click counter
+  timer.innerText = timeRemaining;
+  ticker.innerText = totalClicks;
+
+  // Start countdown timer
+  function startCountdown() {
+    return setInterval(() => {
+      timeRemaining--;
+      timer.innerText = timeRemaining;
+      if (timeRemaining === 0) gameOver();
+    }, 1000);
+  }
+
+  // End the game when the timer reaches zero
+  function gameOver() {
+    clearInterval(countdown);
+    document.getElementById("game-over-text").classList.add("visible");
+  }
+
+  // Display a victory message when all cards are matched
+  function victory() {
+    clearInterval(countdown);
+    document.getElementById("victory-text").classList.add("visible");
+  }
+
+  // Hide all cards
+  function hideCards(cardsArray) {
+    cardsArray.forEach((card) => {
+      card.classList.remove("visible");
+      card.classList.remove("matched");
     });
   }
-});
+
+  // Flip a card and check for a match
+  function flipCard(card) {
+    if (canFlipCard(card)) {
+      totalClicks++;
+      ticker.innerText = totalClicks;
+      card.classList.add("visible");
+
+      if (cardToCheck) {
+        checkForCardMatch(card);
+      } else {
+        cardToCheck = card;
+      }
+    }
+  }
+
+  // Check if two flipped cards match
+  function checkForCardMatch(card) {
+    if (getCardType(card) === getCardType(cardToCheck))
+      cardMatch(card, cardToCheck);
+    else cardMismatch(card, cardToCheck);
+
+    cardToCheck = null;
+  }
+
+  // Handle case where two flipped cards match
+  function cardMatch(card1, card2) {
+    matchedCards.push(card1);
+    matchedCards.push(card2);
+    card1.classList.add("matched");
+    card2.classList.add("matched");
+    if (matchedCards.length === cardsArray.length) victory();
+  }
+
+  // Handle case where two flipped cards do not match
+  function cardMismatch(card1, card2) {
+    busy = true;
+    setTimeout(() => {
+      card1.classList.remove("visible");
+      card2.classList.remove("visible");
+      busy = false;
+    }, 1000);
+  }
+
+  // Shuffle cards using the Fisher-Yates algorithm
+  function shuffleCards(cardsArray) {
+    for (let i = cardsArray.length - 1; i > 0; i--) {
+      let randIndex = Math.floor(Math.random() * (i + 1));
+      cardsArray[randIndex].style.order = i;
+      cardsArray[i].style.order = randIndex;
+    }
+  }
+
+  // Get the type of a card by checking its image source
+  function getCardType(card) {
+    return card.getElementsByClassName("card-value")[0].src;
+  }
+
+  // Check if a card can be flipped
+
+  function canFlipCard(card) {
+    return !busy && !matchedCards.includes(card) && card !== cardToCheck;
+  }
+
+  cardsArray.forEach((card) => {
+    card.addEventListener("click", () => {
+      flipCard(card);
+    });
+  });
+}
+
+function ready() {
+  let overlays = Array.from(document.getElementsByClassName("overlay-text"));
+  let cards = Array.from(document.getElementsByClassName("card"));
+
+  overlays.forEach((overlay) => {
+    overlay.addEventListener("click", () => {
+      overlay.classList.remove("visible");
+      startGame(cards, 100);
+    });
+  });
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
 
 /**
  * Removes the initial message
  * return the cards on the click event
  */
-function startGame() {}
 
 /**
  * shuffle the cards
  */
-function mixCards() {}
 
 /**
  * Check if the cards match
  * If they match, leave them turned
  * If they do not match, they return to the initial state
  */
-function chechMatch() {}
 
 /**
  * It counts the number of clicks and displays them in Flip
  */
-function flipNr() {}
 
 /**
  * At the beginning of the game it displays a number of seconds
  * That will be the time in which all the cards must be turned over
  * To win the game
  */
-function timmer() {}
 
 /**
  * It displays a text that confirms winning the game
  * and the possibility of restarting the game
  */
-function winText() {}
 
 /**
  * It displays a text that confirms loosing the game
  * and the possibility of restarting the game
  */
-function looseText() {}
-
-function timmer() {}
