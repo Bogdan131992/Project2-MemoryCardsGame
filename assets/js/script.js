dbc6b0d0d4a64b78febc5ffd478fa0145279d256
Bogdan Viorel Burlă <bogdanviorel13@gmail.com>
2023-04-26 09:12:29 +0000

    Add the function structure in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,59 @@
-console.log("HI!");
+//
+//Make sure that DOM is loading before the game
+//Get the cards and messages
+
+document.addEventListener("DOMContentLoaded", function () {
+    let overlayText = document.getElementsByClassName("overlay-text");
+    let cards = document.getElementsByClassName("card");
+    for (let card of cards) {
+      card.addEventListener("click", function () {
+        
+      }
+      ));
+    })
+  });
+  
+  /**
+   * Removes the initial message
+   * return the cards on the click event
+   */
+  function startGame() {}
+  
+  /**
+   * shuffle the cards
+   */
+  function mixCards() {}
+  
+  /**
+   * Check if the cards match
+   * If they match, leave them turned
+   * If they do not match, they return to the initial state
+   */
+  function chechMatch() {}
+  
+  /**
+   * It counts the number of clicks and displays them in Flip
+   */
+  function flipNr() {}
+  
+  /**
+   * At the beginning of the game it displays a number of seconds
+   * That will be the time in which all the cards must be turned over
+   * To win the game
+   */
+  function timmer() {}
+  
+  /**
+   * It displays a text that confirms winning the game
+   * and the possibility of restarting the game
+   */
+  function winText() {}
+  
+  /**
+   * It displays a text that confirms loosing the game
+   * and the possibility of restarting the game
+   */
+  function looseText() {}
+  
+  function timmer() {}
+  
