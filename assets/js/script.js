a363fe2f3d72f8450d74619acbf69e49884ccc1a
Bogdan Viorel Burlă <bogdanviorel13@gmail.com>
2023-04-26 09:54:51 +0000

    defining the first functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,57 +3,62 @@
 //Get the cards and messages
 
 document.addEventListener("DOMContentLoaded", function () {
-    let overlayText = document.getElementsByClassName("overlay-text");
-    let cards = document.getElementsByClassName("card");
-    for (let card of cards) {
-      card.addEventListener("click", function () {
-        
-      }
-      ));
-    })
-  });
-  
-  /**
-   * Removes the initial message
-   * return the cards on the click event
-   */
-  function startGame() {}
-  
-  /**
-   * shuffle the cards
-   */
-  function mixCards() {}
-  
-  /**
-   * Check if the cards match
-   * If they match, leave them turned
-   * If they do not match, they return to the initial state
-   */
-  function chechMatch() {}
-  
-  /**
-   * It counts the number of clicks and displays them in Flip
-   */
-  function flipNr() {}
-  
-  /**
-   * At the beginning of the game it displays a number of seconds
-   * That will be the time in which all the cards must be turned over
-   * To win the game
-   */
-  function timmer() {}
-  
-  /**
-   * It displays a text that confirms winning the game
-   * and the possibility of restarting the game
-   */
-  function winText() {}
-  
-  /**
-   * It displays a text that confirms loosing the game
-   * and the possibility of restarting the game
-   */
-  function looseText() {}
-  
-  function timmer() {}
-  
+  let overlayText = document.getElementsByClassName("overlay-text");
+  overlayText.classList.remove("visible");
+
+  //   overlayText.addEventListener("click", function () {
+  //     alert("You cliked a card!");
+  //   });
+
+  let cards = document.getElementsByClassName("card");
+  for (let card of cards) {
+    card.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "card")
+        alert("You cliked a card!");
+    });
+  }
+});
+
+/**
+ * Removes the initial message
+ * return the cards on the click event
+ */
+function startGame() {}
+
+/**
+ * shuffle the cards
+ */
+function mixCards() {}
+
+/**
+ * Check if the cards match
+ * If they match, leave them turned
+ * If they do not match, they return to the initial state
+ */
+function chechMatch() {}
+
+/**
+ * It counts the number of clicks and displays them in Flip
+ */
+function flipNr() {}
+
+/**
+ * At the beginning of the game it displays a number of seconds
+ * That will be the time in which all the cards must be turned over
+ * To win the game
+ */
+function timmer() {}
+
+/**
+ * It displays a text that confirms winning the game
+ * and the possibility of restarting the game
+ */
+function winText() {}
+
+/**
+ * It displays a text that confirms loosing the game
+ * and the possibility of restarting the game
+ */
+function looseText() {}
+
+function timmer() {}
