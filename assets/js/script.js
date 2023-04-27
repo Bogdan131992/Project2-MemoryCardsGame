d6e55d383686ac0b86ab6053f816d587cc741124
Bogdan Viorel Burlă <bogdanviorel13@gmail.com>
2023-04-27 20:29:43 +0000

    Add coments in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,7 +131,7 @@ function startGame(cardsArray, totalTime) {
 }
 
 /**
- * Attaches a click event listener to each overlay and calls the startGame function when clicked.
+ * Attaches a click event listener to each overlay text and calls the startGame function when clicked.
  */
 function ready() {
   // Get all elements with class "overlay-text" and convert them into an array
